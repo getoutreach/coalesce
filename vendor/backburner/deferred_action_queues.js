e843cf71b9ade885be295e79d71cfe3681255843
Bob Nisco <bobnisco@gmail.com>
2017-11-20 14:51:58 -0800

    Remove bower

---
+++ vendor/backburner/deferred_action_queues.js
@@ -0,0 +1,114 @@
+import Queue from './queue';
+
+function DeferredActionQueues(queueNames, options) {
+  var queues = (this.queues = {});
+  this.queueNames = queueNames = queueNames || [];
+
+  var queueName;
+
+  for (var i = 0, l = queueNames.length; i < l; i++) {
+    queueName = queueNames[i];
+    queues[queueName] = new Queue(this, queueName, options[queueName]);
+  }
+}
+
+DeferredActionQueues.prototype = {
+  queueNames: null,
+  queues: null,
+
+  schedule: function(queueName, target, method, args, onceFlag, stack) {
+    var queues = this.queues,
+      queue = queues[queueName];
+
+    if (!queue) {
+      throw new Error('You attempted to schedule an action in a queue (' + queueName + ") that doesn't exist");
+    }
+
+    if (onceFlag) {
+      return queue.pushUnique(target, method, args, stack);
+    } else {
+      return queue.push(target, method, args, stack);
+    }
+  },
+
+  flush: function() {
+    var queues = this.queues,
+      queueNames = this.queueNames,
+      queueName,
+      queue,
+      queueItems,
+      priorQueueNameIndex,
+      queueNameIndex = 0,
+      numberOfQueues = queueNames.length;
+
+    outerloop: while (queueNameIndex < numberOfQueues) {
+      queueName = queueNames[queueNameIndex];
+      queue = queues[queueName];
+      queueItems = queue._queueBeingFlushed = queue._queue.slice();
+      queue._queue = [];
+
+      var options = queue.options,
+        before = options && options.before,
+        after = options && options.after,
+        target,
+        method,
+        args,
+        stack,
+        queueIndex = 0,
+        numberOfQueueItems = queueItems.length;
+
+      if (numberOfQueueItems && before) {
+        before();
+      }
+      while (queueIndex < numberOfQueueItems) {
+        target = queueItems[queueIndex];
+        method = queueItems[queueIndex + 1];
+        args = queueItems[queueIndex + 2];
+        stack = queueItems[queueIndex + 3]; // Debugging assistance
+
+        if (typeof method === 'string') {
+          method = target[method];
+        }
+
+        // method could have been nullified / canceled during flush
+        if (method) {
+          // TODO: error handling
+          if (args && args.length > 0) {
+            method.apply(target, args);
+          } else {
+            method.call(target);
+          }
+        }
+
+        queueIndex += 4;
+      }
+      queue._queueBeingFlushed = null;
+      if (numberOfQueueItems && after) {
+        after();
+      }
+
+      if ((priorQueueNameIndex = indexOfPriorQueueWithActions(this, queueNameIndex)) !== -1) {
+        queueNameIndex = priorQueueNameIndex;
+        continue outerloop;
+      }
+
+      queueNameIndex++;
+    }
+  }
+};
+
+function indexOfPriorQueueWithActions(daq, currentQueueIndex) {
+  var queueName, queue;
+
+  for (var i = 0, l = currentQueueIndex; i <= l; i++) {
+    queueName = daq.queueNames[i];
+    queue = daq.queues[queueName];
+    if (queue._queue.length) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+export default DeferredActionQueues;
---
+++ vendor/backburner/queue.js
@@ -0,0 +1,121 @@
+function Queue(daq, name, options) {
+  this.daq = daq;
+  this.name = name;
+  this.options = options;
+  this._queue = [];
+}
+
+Queue.prototype = {
+  daq: null,
+  name: null,
+  options: null,
+  _queue: null,
+
+  push: function(target, method, args, stack) {
+    var queue = this._queue;
+    queue.push(target, method, args, stack);
+    return { queue: this, target: target, method: method };
+  },
+
+  pushUnique: function(target, method, args, stack) {
+    var queue = this._queue,
+      currentTarget,
+      currentMethod,
+      i,
+      l;
+
+    for (i = 0, l = queue.length; i < l; i += 4) {
+      currentTarget = queue[i];
+      currentMethod = queue[i + 1];
+
+      if (currentTarget === target && currentMethod === method) {
+        queue[i + 2] = args; // replace args
+        queue[i + 3] = stack; // replace stack
+        return { queue: this, target: target, method: method };
+      }
+    }
+
+    this._queue.push(target, method, args, stack);
+    return { queue: this, target: target, method: method };
+  },
+
+  // TODO: remove me, only being used for Ember.run.sync
+  flush: function() {
+    var queue = this._queue,
+      options = this.options,
+      before = options && options.before,
+      after = options && options.after,
+      target,
+      method,
+      args,
+      stack,
+      i,
+      l = queue.length;
+
+    if (l && before) {
+      before();
+    }
+    for (i = 0; i < l; i += 4) {
+      target = queue[i];
+      method = queue[i + 1];
+      args = queue[i + 2];
+      stack = queue[i + 3]; // Debugging assistance
+
+      // TODO: error handling
+      if (args && args.length > 0) {
+        method.apply(target, args);
+      } else {
+        method.call(target);
+      }
+    }
+    if (l && after) {
+      after();
+    }
+
+    // check if new items have been added
+    if (queue.length > l) {
+      this._queue = queue.slice(l);
+      this.flush();
+    } else {
+      this._queue.length = 0;
+    }
+  },
+
+  cancel: function(actionToCancel) {
+    var queue = this._queue,
+      currentTarget,
+      currentMethod,
+      i,
+      l;
+
+    for (i = 0, l = queue.length; i < l; i += 4) {
+      currentTarget = queue[i];
+      currentMethod = queue[i + 1];
+
+      if (currentTarget === actionToCancel.target && currentMethod === actionToCancel.method) {
+        queue.splice(i, 4);
+        return true;
+      }
+    }
+
+    // if not found in current queue
+    // could be in the queue that is being flushed
+    queue = this._queueBeingFlushed;
+    if (!queue) {
+      return;
+    }
+    for (i = 0, l = queue.length; i < l; i += 4) {
+      currentTarget = queue[i];
+      currentMethod = queue[i + 1];
+
+      if (currentTarget === actionToCancel.target && currentMethod === actionToCancel.method) {
+        // don't mess with array during flush
+        // just nullify the method
+        queue[i + 1] = null;
+        return true;
+      }
+    }
+  }
+};
+
+export default Queue;
